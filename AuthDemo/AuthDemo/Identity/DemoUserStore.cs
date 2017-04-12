4749129237c0b1f57e429e2acbc9fd03e146844b
Bentley, Matt (UK - London) <matthew_j_bentley@hotmail.com>
2017-04-12 22:28:34 +0100

    =v1.0

---
+++ AuthDemo/AuthDemo/Identity/DemoUserStore.cs
@@ -0,0 +1,116 @@
+﻿using AuthDemo.Models;
+using AuthDemo.Repositories;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AuthDemo
+{
+    public class DemoUserStore : IUserStore<ApplicationUser>,
+                                    IUserPasswordStore<ApplicationUser>
+    {
+        private IUsersRepository _repo;
+
+        public DemoUserStore(IUsersRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IdentityResult> CreateAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            user.UserId = Guid.NewGuid().ToString();
+
+            await _repo.Add(user);
+
+            return await Task.FromResult(IdentityResult.Success);
+        }
+
+        public async Task<IdentityResult> UpdateAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            bool match = await _repo.Update(user);
+            if (match)
+            {
+                return await Task.FromResult(IdentityResult.Success);
+            }
+            else
+            {
+                return await Task.FromResult(IdentityResult.Failed());
+            }
+        }
+
+        public async Task<IdentityResult> DeleteAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            bool match = await _repo.Delete(user);
+            if (match)
+            {
+                return await Task.FromResult(IdentityResult.Success);
+            }
+            else
+            {
+                return await Task.FromResult(IdentityResult.Failed());
+            }
+        }
+
+        public async Task<ApplicationUser> FindByIdAsync(string userId, CancellationToken cancellationToken)
+        {
+            var user = await _repo.GetById(userId);
+
+            return await Task.FromResult(user);
+        }
+
+        public async Task<ApplicationUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+        {
+            var user = await _repo.GetByName(normalizedUserName);
+
+            return await Task.FromResult(user);
+        }
+
+        public Task<string> GetUserIdAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.UserId);
+        }
+
+        public Task<string> GetUserNameAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.UserName);
+        }
+
+        public Task<string> GetNormalizedUserNameAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.UserName);
+        }
+
+        public Task<string> GetPasswordHashAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.PasswordHash);
+        }
+
+        public Task<bool> HasPasswordAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.PasswordHash != null);
+        }
+
+        public Task SetUserNameAsync(ApplicationUser user, string userName, CancellationToken cancellationToken)
+        {
+            user.UserName = userName;
+            return Task.FromResult(true);
+        }
+
+        public Task SetNormalizedUserNameAsync(ApplicationUser user, string normalizedName, CancellationToken cancellationToken)
+        {
+            user.UserName = normalizedName;
+            return Task.FromResult(true);
+        }
+
+        public Task SetPasswordHashAsync(ApplicationUser user, string passwordHash, CancellationToken cancellationToken)
+        {
+            user.PasswordHash = passwordHash;
+            return Task.FromResult(true);
+        }      
+
+        public void Dispose() { }
+    }
+}
---
+++ AuthDemo/AuthDemo/Repositories/IUsersRepository.cs
@@ -0,0 +1,17 @@
+﻿using AuthDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthDemo.Repositories
+{
+    public interface IUsersRepository
+    {
+        Task<ApplicationUser> GetById(string id);
+        Task<ApplicationUser> GetByName(string name);
+        Task Add(ApplicationUser user);
+        Task<bool> Update(ApplicationUser user);
+        Task<bool> Delete(ApplicationUser user);
+    }
+}
